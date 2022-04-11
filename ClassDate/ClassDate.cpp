f869f72d2fd243752492ee969bd08742000b32a0
Georgy <geravos2@gmail.com>
2022-04-11 19:43:51 +0300

    добавил 2 конструктора

--- ClassDate/ClassDate.cpp
+++ ClassDate/ClassDate.cpp
@@ -2,18 +2,38 @@
 
 #include <iostream>
 using namespace std;
+
 class Date
 {
-public:
-	Date();
-	~Date();
-
 private:
+    int Day;
+    int Month;
+    int Year;
 
+
+public:
+
+
+    Date(int valueDay, int valueMonth , int valueYear ) {
+        Day = valueDay;
+        Month = valueMonth;
+        Year = valueYear;
+    }
+
+    Date(const Date& other) {
+        this->Day = other.Day;
+        this->Month = other.Month;
+        this->Year = other.Year;
+    }
+
+  
+
+   
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Date firstData(1, 1, 23);
+    Date secondData(firstData);
+    return 0;
 }
-
